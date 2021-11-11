7811e4e19fc60b1ead6974989a82175b9db4721f
Weverson Nascimento <weverson.sn@gmail.com>
2021-11-11 01:52:17 -0300

    Change config file to org

--- init.el
+++ init.el
@@ -1,20 +1,2 @@
-;;; init.el -*- lexical-binding: t; -*-
-(setq orbit/theme           'doom-vibrant
-      orbit/font-family     "Fira Code"
-      orbit/font-size       14
-      orbit/project-folders '("~/dev/nu" "~/dev/projects"))
-
-(add-to-list 'load-path (concat "~/.emacs.d/" "modules"))
-
-(setq gc-cons-threshold (* 50 1000 1000))
-
-(require 'setup)
-(require 'ui)
-(require 'search)
-(require 'project)
-(require 'editor)
-(require 'lang)
-(require 'tools)
-(require 'keybind)
-
-(setq gc-cons-threshold (* 2 1000 1000))
+(require 'org)
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
